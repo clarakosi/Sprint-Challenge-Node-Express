eeebe5b31503a66bcc0604308e08fd6bc911a033
Clara Andrew <candrewwani@gmail.com>
2018-02-16 13:18:15 -0500

    Get request added
    src/app.js: Added a get request with two fetch calls to coindesk.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,35 @@ const fetch = require('node-fetch');
 
 const app = express();
 const PORT = 3000;
-const STATUS_ERRO = 422;
+const STATUS_ERROR = 422;
 
 app.use(bodyParser.json());
 
+app.get('/compare', (req, res) => {
+  const result = {};
+  const date = new Date();
+  date.setDate(date.getDate() - 1);
+  const yesterday = date.toISOString().slice(0,10);
+
+  fetch('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
+  .then(res => res.json())
+  .then(json => {
+    result.Today = json.bpi.USD.rate;
+    result.Today = parseFloat(result.Today.replace(/,/g,''))
+  })
+  .then(() => fetch('https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday')
+    .then(res => res.json())
+    .then(json => {
+      result.Yesterday = json.bpi[yesterday];
+      result.Change = result.Today - result.Yesterday
+      res.json(result)
+    }))
+  .catch(err => {
+    console.log(err);
+    res.status(STATUS_ERROR);
+    res.send('There seems to be an error with your API request.')
+  })
+});
 
 
 app.listen(PORT, err => {
